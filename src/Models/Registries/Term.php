ecb4463c4d054aa90e15e8dfce9b507647aa855d
Marek Woś <m.wos@madkom.pl>
2015-07-28 18:52:13 +0200

    kosmetyka + dodanie doc komentarzy

--- src/Models/Registries/Term.php
+++ src/Models/Registries/Term.php
@@ -7,19 +7,35 @@
  */
 
 namespace Models\Registries;
+
 use Doctrine\Common\Collections\ArrayCollection;
 
 class Term
 {
+    /** @var  integer $termId */
     private $termId;
+
+    /** @var  string $termType */
     private $termType;
+
+    /** @var  \DateTime $dateFrom */
     private $dateFrom;
+
+    /** @var  \DateTime $dateTo */
     private $dateTo;
+
+    /** @var  string $department */
     private $department;
+
+    /** @var  integer $reminder */
     private $reminder;
 
+    /** @var ArrayCollection $cars */
     private $cars;
 
+    /**
+     * @param $cars
+     */
     public function __construct($cars)
     {
         $this->cars = new ArrayCollection();
