ab39025c14c6782dfeb54a7f2f6e32aa290aa6ec
Marek Woś <m.wos@madkom.pl>
2015-07-28 18:55:33 +0200

    kosmetyka + dodanie doc komentarzy

--- src/Models/Registries/CarRegistry/Car.php
+++ src/Models/Registries/CarRegistry/Car.php
@@ -9,20 +9,45 @@
 namespace Models\Registries\CarRegistry;
 use Doctrine\Common\Collections\ArrayCollection;
 
+/**
+ * Class Car
+ * @package Models\Registries\CarRegistry
+ */
 class Car
 {
+    /** @var  integer $carId */
     private $carId;
+
+    /** @var  string $brand */
     private $brand;
+
+    /** @var  string $model */
     private $model;
+
+    /** @var  string $registrationNumber */
     private $registrationNumber;
+
+    /** @var  string $insurer */
     private $insurer;
+
+    /** @var  string $others */
     private $others;
+
+    /** @var  string $attachments */
     private $attachments;
+
+    /** @var  string $createdBy */
     private $createdBy;
+
+    /** @var  integer $registryId */
     private $registryId;
 
+    /** @var ArrayCollection  $terms */
     private $terms;
 
+    /**
+     * @param $terms
+     */
     public function __construct($terms)
     {
         $this->terms = new ArrayCollection();
