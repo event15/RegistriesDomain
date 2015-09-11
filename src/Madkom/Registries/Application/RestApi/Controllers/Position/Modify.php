574859a5952cc4e469686f3b9136e6ce649cad00
Marek Woś <m.wos@madkom.pl>
2015-09-11 11:19:23 +0200

    umożliwienie edytowania metatdanych w wybranej pozycji.

--- src/Madkom/Registries/Application/RestApi/Controllers/Position/Modify.php
+++ src/Madkom/Registries/Application/RestApi/Controllers/Position/Modify.php
@@ -3,8 +3,37 @@
 namespace Madkom\Registries\Application\RestApi\Controllers\Position;
 
 use Madkom\Registries\Application\RestApi\Controllers\ControllerHelper;
+use Madkom\Registries\Domain\EmptyRegistryException;
+use Silex\Application;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class Modify extends ControllerHelper
 {
+    public function positionById(Application $app, Request $request, $id, $positionId)
+    {
+        $helper = new ControllerHelper();
+        $currentRegistry = $helper->findAndCheckRegistry($app, $id);
 
+        /** @var \Madkom\Registries\Domain\Car\Car $getElement */
+        $getElement = $app['repositories.position']->find('Madkom\\Registries\\Domain\\Car\\Car', $positionId);
+
+        if ($getElement === null) {
+            throw new EmptyRegistryException('Wybrany element nie istnieje.');
+        }
+
+
+        $getElement->changeBrand($request->get('brand'));
+        $getElement->changeModel($request->get('model'));
+        $getElement->changeOthers($request->get('others'));
+        $getElement->changeRegistrationNumber($request->get('registrationNumber'));
+
+
+        $app['repositories.position']->prepareToSave($getElement);
+        $app['repositories.registry']->save($currentRegistry);
+
+
+        return new Response('OK', 201);
+    }
 }
